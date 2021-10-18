d1a1c1bb6ae548a546877f7b8600c69a1288ee68
IvanBelockobylscky <92727992+IvanBelockobylscky@users.noreply.github.com>
2021-10-18 15:19:53 +0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            Console.WriteLine("Введите число:");
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = (int)num1;
+            num2 = (num1 - num2)*10;
+            num2 = (int)num2;
+            Console.WriteLine($"Первая цифра после запятой: {num2}");
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            Console.WriteLine("Введите прошедшее время в секундах:");
+            int time = int.Parse(Console.ReadLine());
+            int hour = time / 3600;
+            int min = ((time - hour * 3600) / 60);
+            int sec = time - hour * 3600 - min * 60;
+            Console.Clear();
+            Console.WriteLine($"{time} секунд = {hour} часов, {min} минут и {sec} секунд");
+        }
+    }
+}
