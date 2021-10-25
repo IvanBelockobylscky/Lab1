58048da76a694068801f3c9b48a3bd6a3609a761
IvanBelockobylscky <92727992+IvanBelockobylscky@users.noreply.github.com>
2021-10-25 14:28:16 +0300

    Add files via upload

---
+++ 6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, x, y, z, c, ans;
+            Console.WriteLine("Введите четырехзначное число:");
+            num= int.Parse(Console.ReadLine());
+            if (num <1000 ^ num > 9999)
+            {
+                do
+                {
+                    Console.WriteLine("Число не четырехзначное, попытка номер два");
+                    num = int.Parse(Console.ReadLine());
+                } while (num < 1000 ^ num > 9999);
+            }
+            {
+                x = num / 1000;
+                y = (num - (x * 1000)) / 100;
+                z = (num - (x * 1000) - (y * 100)) / 10;
+                c = (num - (x * 1000)) - (y * 100) - (z * 10);
+                ans = x * y * z * c;
+                Console.WriteLine($"Произведение цифр введенного четырехзначного числа:{ans}");
+            }
+        }
+    }
+}
+
