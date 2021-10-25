7cbed3bc4e1cc18fc2df3b9edc236d5bd8bdcef8
IvanBelockobylscky <92727992+IvanBelockobylscky@users.noreply.github.com>
2021-10-25 14:30:33 +0300

    Add files via upload

---
+++ 8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace zadanie_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, ans;
+            Console.WriteLine("Введите значение x");
+            a = double.Parse(Console.ReadLine());
+            ans = (((3 * a - 5) * a + 2) * a - 1) * a + 7;
+            Console.WriteLine($"Ответ: {ans}");
+        }
+    }
+}
